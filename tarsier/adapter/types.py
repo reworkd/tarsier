7f502db58883537d83fcb210375479d6e7d4ccd5
awtkns <32209255+awtkns@users.noreply.github.com>
2023-11-14 15:08:37 -0800

    ♻️ Refactor code running abstractions

--- tarsier/adapter/types.py
+++ tarsier/adapter/types.py
@@ -1,7 +1,13 @@
-from typing import Union
+from typing import TypedDict, Union
 
 from playwright.async_api import Page as PageAsync
 from playwright.sync_api import Page as PageSync
 from selenium.webdriver.remote.webdriver import WebDriver
 
 AnyDriver = Union[WebDriver, PageSync, PageAsync]
+
+
+class ViewPortSize(TypedDict):
+    width: int
+    height: int
+    content_height: int
--- tarsier/core.py
+++ tarsier/core.py
@@ -16,30 +16,30 @@ class Tarsier(ITarsier):
         with open(self._JS_TAG_UTILS, "r") as f:
             self._js_utils = f.read()
 
-    async def page_to_image(self, driver: AnyDriver, tag_text_elements: bool = False) -> Tuple[bytes, Dict[int, str]]:
+    async def page_to_image(
+        self, driver: AnyDriver, tag_text_elements: bool = False
+    ) -> Tuple[bytes, Dict[int, str]]:
         adapter = adapter_factory(driver)
         tag_to_xpath = await self._tag_page(adapter, tag_text_elements)
         screenshot = await self._take_screenshot(adapter)
         await self._remove_tags(adapter)
         return screenshot, tag_to_xpath
 
-    async def page_to_text(self, driver: AnyDriver, tag_text_elements: bool = False) -> Tuple[str, TagToXPath]:
+    async def page_to_text(
+        self, driver: AnyDriver, tag_text_elements: bool = False
+    ) -> Tuple[str, TagToXPath]:
         image, tag_to_xpath = await self.page_to_image(driver, tag_text_elements)
         page_text = self._run_ocr(image)
         return page_text, tag_to_xpath
 
     @staticmethod
     async def _take_screenshot(adapter: BrowserAdapter) -> bytes:
-        # TODO: scroll & stitch here, don't do viewport resizing
-        script = "() => [window.innerWidth, window.innerHeight, document.documentElement.scrollHeight];"
-        if isinstance(adapter, SeleniumAdapter):
-            script = f"return [window.innerWidth, window.innerHeight, document.documentElement.scrollHeight];"
+        viewport = await adapter.get_viewport_size()
+        default_width = viewport["width"]
 
-        default_width, default_height, content_height = await adapter.run_js(script)
-
-        await adapter.set_viewport_size(default_width, content_height)
+        await adapter.set_viewport_size(default_width, viewport["content_height"])
         screenshot = await adapter.take_screenshot()
-        await adapter.set_viewport_size(default_width, default_height)
+        await adapter.set_viewport_size(default_width, viewport["height"])
 
         return screenshot
 
@@ -53,19 +53,12 @@ class Tarsier(ITarsier):
     ) -> Dict[int, str]:
         await adapter.run_js(self._js_utils)
 
-        script = f"tagifyWebpage({str(tag_text_elements).lower()});"
-        if isinstance(adapter, SeleniumAdapter):
-            script = f"return window.{script}"
-
+        script = f"return window.tagifyWebpage({str(tag_text_elements).lower()});"
         tag_to_xpath = await adapter.run_js(script)
+
         return {int(key): value for key, value in tag_to_xpath.items()}
 
     async def _remove_tags(self, adapter: BrowserAdapter) -> None:
-        # await adapter.run_js(self._js_utils)
-
-        script = "removeTags();"
-        if isinstance(adapter, SeleniumAdapter):
-            script = f"return window.{script}"
+        script = "return window.removeTags();"
 
         await adapter.run_js(script)
-        return None
