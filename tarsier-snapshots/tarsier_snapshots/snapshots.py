0aba6470bb69f021f4bb56b4580186f3dfc3a07d
asim-shrestha <asim.shrestha@hotmail.com>
2024-02-02 14:11:58 -0800

    🍌 Lint

--- tarsier-snapshots/tarsier_snapshots/snapshots.py
+++ tarsier-snapshots/tarsier_snapshots/snapshots.py
@@ -1,36 +1,45 @@
 import asyncio
 import os
 from pathlib import Path
+from typing import Dict
 
+from bananalyzer import Example
 from bananalyzer.data.examples import get_training_examples
 from dotenv import load_dotenv
-from playwright.async_api import async_playwright
+from playwright.async_api import Browser, async_playwright
 
-from tarsier import Tarsier, GoogleVisionOCRService
+from tarsier import GoogleVisionOCRService, Tarsier
 
 load_dotenv()
 
 
-def google_creds() -> dict:
+def google_creds() -> Dict[str, str]:
     return {
-        "type": os.getenv("TYPE"),
-        "project_id": os.getenv("PROJECT_ID"),
-        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-        "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
-        "client_email": os.getenv("CLIENT_EMAIL"),
-        "client_id": os.getenv("CLIENT_ID"),
-        "auth_uri": os.getenv("AUTH_URI"),
-        "token_uri": os.getenv("TOKEN_URI"),
-        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-        "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+        "type": os.getenv("TYPE") or "",
+        "project_id": os.getenv("PROJECT_ID") or "",
+        "private_key_id": os.getenv("PRIVATE_KEY_ID") or "",
+        "private_key": (os.getenv("PRIVATE_KEY") or "").replace("\\n", "\n"),
+        "client_email": os.getenv("CLIENT_EMAIL") or "",
+        "client_id": os.getenv("CLIENT_ID") or "",
+        "auth_uri": os.getenv("AUTH_URI") or "",
+        "token_uri": os.getenv("TOKEN_URI") or "",
+        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL") or "",
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL") or "",
+        "universe_domain": os.getenv("UNIVERSE_DOMAIN") or "",
     }
 
 
 examples = get_training_examples()
 
 
-async def snapshot_example(index: int, semaphore: asyncio.Semaphore, browser, example, snapshots_path, tarsier):
+async def snapshot_example(
+    index: int,
+    semaphore: asyncio.Semaphore,
+    browser: Browser,
+    example: Example,
+    snapshots_path: Path,
+    tarsier: Tarsier,
+) -> None:
     async with semaphore:
         page = await browser.new_page()
         example_path = snapshots_path / example.id
@@ -53,7 +62,7 @@ async def snapshot_example(index: int, semaphore: asyncio.Semaphore, browser, ex
         print(f"Finished snapshotting {example.id}")
 
 
-async def generate_snapshots():
+async def generate_snapshots() -> None:
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         snapshots_path = Path(__file__).parent.parent / "snapshots"
@@ -62,9 +71,11 @@ async def generate_snapshots():
 
         tasks = [
             snapshot_example(i, semaphore, browser, example, snapshots_path, tarsier)
-            for i, example in enumerate(examples) if example.source == "mhtml"
+            for i, example in enumerate(examples)
+            if example.source == "mhtml"
         ]
         await asyncio.gather(*tasks)
+
 
 if __name__ == "__main__":
     asyncio.run(generate_snapshots())
