2e5f7371886643e3555b72775d5244c02fe1daae
seanmcguire12 <seanmcguire1@outlook.com>
2024-08-07 12:16:33 -0700

    reformat

--- tests/test_namespace.py
+++ tests/test_namespace.py
@@ -6,11 +6,13 @@ import pytest
 @pytest.mark.parametrize(
     "html_file, expected_xpath",
     [
-        ("mock_html/namespace.html", "//html/body/*[name()=\"sc:visitoridentification\"]/div"),
+        (
+            "mock_html/namespace.html",
+            '//html/body/*[name()="sc:visitoridentification"]/div',
+        ),
     ],
 )
 async def test_xpath(tarsier, async_page, html_file, expected_xpath):
-
     html_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), html_file))
     await async_page.goto(f"file://{html_file_path}")
 
