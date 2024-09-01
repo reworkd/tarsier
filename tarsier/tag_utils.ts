bc1912b3e6dc6845d01c0b906ea988718f9ec56c
Sean McGuire <75873287+seanmcguire12@users.noreply.github.com>
2024-08-31 18:36:20 -0700

    ⏮️ fix bug where remove tags was not removing tags in iframes (#145)
    * ⏮️ fix bug where remove tags was not removing tags in iframes
    
    * address comment
    
    * bump version

--- tarsier/tag_utils.ts
+++ tarsier/tag_utils.ts
@@ -614,8 +614,10 @@ const shrinkCollidingTags = () => {
 };
 
 window.removeTags = () => {
-  const tags = document.querySelectorAll(tarsierSelector);
-  tags.forEach((tag) => tag.remove());
+  getAllElementsInAllFrames()
+    .filter((element) => element.matches(tarsierSelector))
+    .forEach((tag) => tag.remove());
+
   showMapElements();
 };
 
